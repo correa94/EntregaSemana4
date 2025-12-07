548617eeede68c6afa6204a12df1dcba6e2ac6c4
lean <leancorrea.t@gmail.com>
2025-12-07 19:40:29 -0300

    Entrega final

--- ejemplo/js/carrito.js
+++ ejemplo/js/carrito.js
@@ -12,12 +12,79 @@ function renderCarrito(cartItems) {
             <h4>Precio: $${producto.precio}</h4>
             <h4>Marca: ${producto.marca}</h4>
             <h6>${producto.descripcion}</h6>
-            <p><strong>Cantidad:</strong> ${producto.cantidad}</p>
+            <p><strong>Cantidad:</strong> <button onclick ="restarCantidad(${producto.id})"> - </button> ${producto.cantidad} <button onclick ="sumarCantidad(${producto.id})"> + </button></p>
+
 
         </div>
     </div>`;
+    
     cartContainer.appendChild(card);
   });
 }
 
 renderCarrito(cartStorage);
+function calcularTotal () {
+  let totalDiv = document.getElementById("totales");
+let totalAmount = 0;
+let cartStorage = JSON.parse(localStorage.getItem("cartProducts")) || [];
+cartStorage.forEach((producto) => {
+  totalAmount += producto.precio * producto.cantidad;
+});
+totalDiv.innerHTML = `<h3>Total a pagar: $${totalAmount}</h3>`;
+}
+calcularTotal();
+
+function sumarCantidad(productoId) {
+  let cartStorage = JSON.parse(localStorage.getItem("cartProducts"));
+  const producto = cartStorage.find((prod) => prod.id === productoId);
+  producto.cantidad += 1;
+  localStorage.setItem("cartProducts", JSON.stringify(cartStorage));
+  cartContainer.innerHTML = "";
+  renderCarrito(cartStorage);
+  calcularTotal();
+  Toastify({
+  text: "Sumaste un producto",
+  close: true,
+  duration: 3000,
+  gravity: "top",
+  position: "right",
+}).showToast();
+}
+
+function restarCantidad(productoId) {
+  let cartStorage = JSON.parse(localStorage.getItem("cartProducts"));
+  const producto = cartStorage.find((prod) => prod.id === productoId);
+  if (producto.cantidad > 1) {
+    producto.cantidad -= 1;
+  } else {
+    cartStorage = cartStorage.filter((prod) => prod.id !== productoId);
+  }
+  localStorage.setItem("cartProducts", JSON.stringify(cartStorage));
+  cartContainer.innerHTML = "";
+  renderCarrito(cartStorage);
+  calcularTotal();
+  Toastify({
+  text: "Restaste un producto",
+  close: true,
+  duration: 3000,
+  gravity: "top",
+  position: "right",
+  style: {
+    background: "red",
+  },
+}).showToast();
+}
+
+function completarCompra(){
+  Swal.fire({
+    title: '¡Compra realizada con exito!',
+    text: "Gracias por su compra.",
+    icon: 'success',
+    confirmButtonText: 'Cerrar'
+  }).then(() => {
+    localStorage.removeItem("cartProducts");
+    cartContainer.innerHTML = "";
+    calcularTotal();
+  });
+}
+
--- ejemplo/js/main.js
+++ ejemplo/js/main.js
@@ -1,74 +1,11 @@
-const productos = [
-  {
-    id: 1,
-    img: "../img/img mancuerna.jpg",
-    nombre: "Mancuernas Ajustables",
-    precio: 25000,
-    categoria: "Pesas",
-    marca: "PowerFit",
-    descripcion:
-      "Mancuernas ajustables de 2.5 a 24 kg con sistema de cambio rápido.",
-  },
-  {
-    id: 2,
-    img: "../img/bici gym.jpg",
-    nombre: "Bicicleta Estática",
-    precio: 85000,
-    categoria: "Cardio",
-    marca: "NordicTrack",
-    descripcion:
-      "Bicicleta con pantalla digital y resistencia magnética ajustable.",
-  },
-  {
-    id: 3,
-    img: "../img/colchoneta yoga.jpg",
-    nombre: "Colchoneta de Yoga",
-    precio: 7000,
-    categoria: "Yoga",
-    marca: "Adidas",
-    descripcion: "Colchoneta antideslizante de 6 mm ideal para yoga o pilates.",
-  },
-  {
-    id: 4,
-    img: "../img/banco de pesas.avif",
-    nombre: "Banco de Pesas",
-    precio: 30000,
-    categoria: "Musculación",
-    marca: "BodyStrong",
-    descripcion:
-      "Banco reclinable de acero reforzado para entrenamiento con mancuernas o barra.",
-  },
-  {
-    id: 5,
-    img: "../img/cinta de correr.avif",
-    nombre: "Cinta de Correr",
-    precio: 180000,
-    categoria: "Cardio",
-    marca: "ProForm",
-    descripcion:
-      "Cinta eléctrica con 12 programas automáticos y monitor de frecuencia cardíaca.",
-  },
-  {
-    id: 6,
-    img: "../img/bandas elasticas.webp",
-    nombre: "Set de Bandas Elásticas",
-    precio: 9000,
-    categoria: "Entrenamiento Funcional",
-    marca: "Reebok",
-    descripcion:
-      "Set de 5 bandas de diferentes resistencias con asas y bolsa de transporte.",
-  },
-  {
-    id: 7,
-    img:"../img/barra olimpica.jpg",
-    nombre: "Barra Olímpica",
-    precio: 45000,
-    categoria: "Pesas",
-    marca: "IronMax",
-    descripcion:
-      "Barra de acero cromado de 20 kg, ideal para powerlifting y crossfit.",
-  },
-];
+let productos = [];
+fetch("./data/MainProductos.json")
+  .then((response) => response.json())
+  .then((data) => {
+    renderProductos(data);
+    productos = data;
+  });
+
 let cartProducts = [];
 let productsContainer = document.getElementById("products-container");
 
@@ -83,7 +20,6 @@ function renderProductos(productsArray) {
             <h4>Precio: $${producto.precio}</h4>
             <h4>Marca: ${producto.marca}</h4>
             <h6>${producto.descripcion}</h6>
-            <p><strong>Cantidad:</strong> ${producto.cantidad}</p>
                     <button class="productoAgregar" id="${producto.id}">Agregar</button>
 
         </div>
@@ -92,8 +28,6 @@ function renderProductos(productsArray) {
   });
   agregarAlCarrito();
 }
-
-renderProductos(productos);
 
 function agregarAlCarrito() {
   const addButton = document.querySelectorAll(".productoAgregar");
@@ -117,10 +51,15 @@ function agregarAlCarrito() {
         selectedProduct.cantidad = 1;
         cartProducts.push(selectedProduct);
       }
+      Toastify({
+        text: "Sumaste " + selectedProduct.nombre + " al carrito",
+        close: true,
+        duration: 3000,
+        gravity: "top",
+        position: "right",
+      }).showToast();
 
       localStorage.setItem("cartProducts", JSON.stringify(cartProducts));
-
-      console.log("Carrito actualizado:", cartProducts);
     };
   });
 }
